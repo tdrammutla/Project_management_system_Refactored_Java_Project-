42622d22b94874a4bd7b36df5d89eefc4726fb63
Tebogo Rammutla <RammutlaT@amexforex.local>
2022-11-02 12:19:01 +0200

    Added new file Architect.java

---
+++ Users/rammutlat.000/Music/ToHub/Architect.java
@@ -0,0 +1,52 @@
+/*
+Architect of the project class
+ */
+
+/**
+ *
+ * @author RammutlaT
+ */
+ //Architect class definition
+public class Architect 
+{
+    //Properties of Architect class
+    private String name;
+    private String surname;
+    private String telephoneNumber;
+    private String emailAddress;
+    private String physicalAddress;
+    
+    /**
+     * Constructor of Architect Class
+     * @param name the architect name to set
+     * @param surname the architect surname to set
+     * @param telephoneNumber the architect telephoneNumber to set
+     * @param emailAddress the architect email address to set
+     * @param physicalAddress the architect physical address to set
+     */
+    public Architect(String name,String surname,String telephoneNumber,String emailAddress,String physicalAddress )
+    {        
+        this.name = name;
+        this.surname = surname;
+        this.telephoneNumber = telephoneNumber;
+        this.emailAddress = emailAddress;
+        this.physicalAddress = physicalAddress;   
+    
+    }  
+    
+  //toString methods to display object 's data
+        /**
+         * toString
+         * @return customer string object
+         */ 
+    public String toString()
+    {
+        
+        String customer = "\nArchitect name : " + name
+                + "\nArchitect surname : " + surname
+                + "\nArchitect telephone number : " + telephoneNumber
+                + "\nArchitect email_address : "  + emailAddress
+                + "\nArchitect physical_address : " + physicalAddress + "\n";
+        return customer;
+    } 
+}
